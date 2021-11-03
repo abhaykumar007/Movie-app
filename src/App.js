3bf832e038482d98543ba9831ee7b12860ddc211
abhay <abhaybhagat2609@gmail.com>
2021-11-03 11:55:46 +0530

    filed uploaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import MovieCard from "./component/movieCard";
+import NavbarComponent from "./component/navbar";
+import { data } from "./data";
 function App() {
+  const [movieList, setMovieList] = useState(data);
+  const [favoriteList, setFavoriteList] = useState([]);
+  const [isFavTab, setFavTab] = useState(false);
+  // console.log(movieList);
+  function handelFavOnClick(movie) {
+    setFavoriteList((prevState) => [...prevState, movie]);
+  }
+  function handelRemoveFavOnClick(movie) {
+    let ref = favoriteList.filter((element) => element !== movie);
+    setFavoriteList(ref);
+  }
+
+  // console.log(favoriteList);
+
+  const listOfMovies = isFavTab ? favoriteList : movieList;
+
+  function favorite(movie) {
+    let index = favoriteList.findIndex((element) => element == movie);
+    if (index != -1) {
+      return true;
+    }
+    return false;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComponent />
+
+      <div className="main">
+        <div className="tabs">
+          <div
+            className={`tab ${isFavTab ? "" : "active-tabs"}`}
+            onClick={() => setFavTab(false)}
+          >
+            Movie
+          </div>
+          <div
+            className={`tab ${isFavTab ? "active-tabs" : ""}`}
+            onClick={() => setFavTab(true)}
+          >
+            Favorite
+          </div>
+        </div>
+
+        <div className="list">
+          {listOfMovies.map((movie, index) => (
+            <MovieCard
+              key={index}
+              movie={movie}
+              handelFavOnClick={handelFavOnClick}
+              handelRemoveFavOnClick={handelRemoveFavOnClick}
+              favorite={favorite(movie)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar(props) {
+  return (
+    <div className="nav">
+      <div className="search-container">
+        <input />
+        <button className="search-btn">Search</button>
+      </div>
+    </div>
+  );
+}
